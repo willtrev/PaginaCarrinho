98b3f5daed5b2bf29c10b8ea6f7667ce21c3ce21
Willian Trevisan <williantrev@gmail.com>
2020-09-18 17:43:57 -0400

    Estruturado e estilizado 'resumo do pedido'

--- src/pages/Carrinho/index.jsx
+++ src/pages/Carrinho/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { FaWhatsapp } from 'react-icons/fa'
-import { MdPerson, MdLocationOn, MdShoppingCart, MdMenu, MdSearch } from 'react-icons/md'
+import { MdPerson, MdLocationOn, MdShoppingCart, MdMenu, MdSearch, MdLocalShipping, MdCreditCard, MdLocalOffer } from 'react-icons/md'
 
 import SuaMarca from '../../assets/SuaMarca.jpg';
 
-import { TextIcon, Boxcontato, Boxmarca, NavBar, Header, TextGroup, MenuGroup, Busca, Section, Total, Centraliza } from './styles';
+import { TextIcon, Boxcontato, Boxmarca, NavBar, Header, TextGroup, MenuGroup, Busca, Section, Total, Shopping, Cart, Resumo, Box, ButtonF } from './styles';
 
 const Carrinho = () => {
   return(
@@ -32,7 +32,40 @@ const Carrinho = () => {
           <Total> <MdShoppingCart size="24px" color="red" /> <p>R$ 49,49</p> </Total>
         </NavBar>
       </Header>
-      <Section></Section>
+      <Section>
+        <TextIcon><MdLocalShipping size="20px" color="grey" /><p>Delivery apenas para Joinville</p></TextIcon>
+        <TextIcon><MdLocalOffer size="20px" color="grey" /><p>Descontos de 10% nas compras acima de R$ 200,00</p></TextIcon>
+        <TextIcon><MdCreditCard size="20px" color="grey" /><p>Pague em até 12x no cartão</p></TextIcon>
+      </Section>
+      <Shopping>
+        <h1>Carrinho</h1>
+        <div>
+          <Cart></Cart>
+          <Resumo>
+            <Box><h1>RESUMO DO PEDIDO</h1></Box>
+            <article>
+              <div>
+              <p>Itens</p>
+              <p>5</p>
+              </div>
+              <div>
+                <p>Total em produtos</p> 
+                <p>R$ 62,62</p>
+              </div>
+              <div>
+                <p>Descontos</p>
+                <p>R$ 0,00</p>
+              </div>
+            </article>
+            <div>
+              <h2>Total</h2> <h2>R$ 62,62</h2>
+            </div>
+            <ButtonF><button>Finalizar a compra</button></ButtonF>
+          </Resumo>
+        </div>
+
+
+      </Shopping>
     </>
 ) 
 }
--- src/pages/Carrinho/styles.js
+++ src/pages/Carrinho/styles.js
@@ -4,23 +4,26 @@ import styled from 'styled-components';
 
 export const Header = styled.header`
   display: flex;
-  flex-direction: column
+  flex-direction: column;
+  position: sticky;
+  top: 0;
+  background-color: #fff
 `;
 
 export const Boxcontato = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-
+  margin: 20px 0 15px 0;
 `;
 
 export const TextIcon = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 55px;
+  height: 20px;
 
-  padding: 20px 0 15px 0;
+  /* padding: 20px 0 15px 0; */
 
   font-size: 12px;
   font-family:'Roboto', sans-serif;
@@ -94,7 +97,7 @@ export const Busca = styled.form`
 
     font-family:'Roboto', sans-serif;
     font-size: 14px;
-    font-weight: medium;
+    font-weight: 500;
   }
 
 `;
@@ -124,4 +127,115 @@ export const Total = styled.div`
 export const Section = styled.section`
   height: 60px;
   background-color: #F5F5F5;
+  display: flex;
+  flex: 1;
+  justify-content: space-evenly;
+  align-items: center;
+
+  p {
+    display: flex;
+    align-items: center;
+    height: 20px;
+    margin: 20px 0 20px 5px;
+    font-family:'Roboto', sans-serif;
+    font-weight: 500;
+    font-size: 12px;
+  }
+`;
+
+/* Estilização do carrinho e resumo da compra */
+
+export const Shopping = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  max-width: 968px;
+  h1 {
+    align-self: center;
+    font-family: 'Montserrat', sans-serif;
+    letter-spacing: -1.5px;
+
+    margin: 30px 0 30px 0;
+    height: 40px;
+    font-size: 32px;
+  }
+
+  div {
+    display: flex;
+    max-width: 968px;
+    
+  }
+`;
+
+export const Cart = styled.section`
+  
+`;
+
+export const Box = styled.div`
+  display: flex;
+  height: 50px;
+  width: 300px;
+  border-bottom: 1px solid grey;
+`; 
+
+export const Resumo = styled.section`
+  width: 300px;
+  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  align-self: flex-end;
+  border: 1px solid grey;
+  
+  div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  h1 {
+    align-self: center;
+    height: 20px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    font-size: 14px;
+    letter-spacing: 0.1px;
+    margin: 0 0 0 20px;
+  }
+  h2 {
+    height: 30px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    font-size: 16px;
+    margin: 0 20px;
+  }
+  p {
+    height: 20px;
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+    
+  }
+
+  article {
+    height: 130px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+`;
+
+export const ButtonF = styled.div`
+  height: 90px;
+  padding: 20px;
+  
+  button {
+    flex: 1;
+    color: white;
+    background-color: #e53935;
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    font-size: 14px;
+    letter-spacing: 0.1px;
+    border: none;
+  }
 `;
