636ef296c3c676a433b1f00c72436157cc5eadf2
Willian Trevisan <williantrev@gmail.com>
2020-09-20 19:15:34 -0400

    get dos produtos do carrinho aplicado

--- src/components/shoppingCart/index.jsx
+++ src/components/shoppingCart/index.jsx
@@ -1,61 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { formatPrice } from '../../util/format';
+
+import api from '../../services/api';
 
 import { Shopping, ProductList, Resumo, Box, ButtonF, Descricao, Title, TextIcon, Contador, ValorTotal } from './styles';
 import { BiComment } from 'react-icons/bi';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 import { IoMdTrash } from 'react-icons/io';
 
+export default class shoppingCart extends Component {
+  state = {
+    products: [],
+  };
 
-const shoppingCart = () => {
-  return(
-    <>
-      <Shopping>
-        <Title>Carrinho</Title>
-        <div>
-          <ProductList>
-            <li>
-              <img src="https://simplest-meuspedidos-arquivos.s3.amazonaws.com/media/imagens_auto/alimentos/arroz_pacote.jpg" alt="a"></img>
-              <Descricao>
-                <h1>REFRIGERANTE 2 LITROS</h1>
-                <p>SKU 123213217</p>
-                <TextIcon> <BiComment size="20px" /> <p>Adicionar observação</p> </TextIcon>
-              </Descricao>
-              <Contador>
-                <button><AiOutlineMinus size="20px" color="grey"/></button>
-                <p>2</p>
-                <button><AiOutlinePlus size="20px"color="red"/></button>
-              </Contador>
-              <ValorTotal>
-                <p>R$ 12,50</p>
-                <IoMdTrash size="20px" color="red" />
-              </ValorTotal>
-            </li>
-          </ProductList>
-          <Resumo>
-            <Box><h1>RESUMO DO PEDIDO</h1></Box>
-            <article>
+  async componentDidMount(){
+    const response = await api.get('/carrinho');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.valor_unitario),
+    }));
+
+    this.setState({ products: data });
+
+  }
+
+
+
+  render () {
+    const { products } = this.state;
+
+    return(
+      <>
+        <Shopping>
+          <Title>Carrinho</Title>
+          <div>
+            <ProductList>
+              { products.map(product => (
+                <li key={product.id}>
+                  <img
+                    src={product.url_imagem}
+                    alt={product.nome}
+                  />
+                  <Descricao>
+                    <h1>{product.nome}</h1>
+                    <p>SKU {product.sku}</p>
+                    <TextIcon> <BiComment size="20px" /> <p>Adicionar observação</p> </TextIcon>
+                  </Descricao>
+                  <Contador>
+                    <button><AiOutlineMinus size="20px" color="grey"/></button>
+                    <p>{product.quantidade}</p>
+                    <button><AiOutlinePlus size="20px"color="red"/></button>
+                  </Contador>
+                  <ValorTotal>
+                    <p>{product.priceFormatted}</p>
+                    <IoMdTrash size="20px" color="red" />
+                  </ValorTotal>
+                </li>
+              )) }
+              
+            </ProductList>
+            <Resumo>
+              <Box><h1>RESUMO DO PEDIDO</h1></Box>
+              <article>
+                <div>
+                <p>Itens</p>
+                <p>5</p>
+                </div>
+                <div>
+                  <p>Total em produtos</p> 
+                  <p>R$ 62,62</p>
+                </div>
+                <div>
+                  <p>Descontos</p>
+                  <p>R$ 0,00</p>
+                </div>
+              </article>
               <div>
-              <p>Itens</p>
-              <p>5</p>
+                <h2>Total</h2> <h2>R$ 62,62</h2>
               </div>
-              <div>
-                <p>Total em produtos</p> 
-                <p>R$ 62,62</p>
-              </div>
-              <div>
-                <p>Descontos</p>
-                <p>R$ 0,00</p>
-              </div>
-            </article>
-            <div>
-              <h2>Total</h2> <h2>R$ 62,62</h2>
-            </div>
-            <ButtonF><button>Finalizar a compra</button></ButtonF>
-          </Resumo>
-        </div>
-      </Shopping>
-    </>
-  ) 
+              <ButtonF><button>Finalizar a compra</button></ButtonF>
+            </Resumo>
+          </div>
+        </Shopping>
+      </>
+    ) 
+  }
 }
-
-export default shoppingCart;
